87af6db946b60fe0820eb1b9ed2a00e4628cafbc
Max Tymchiy <samback@ukr.net>
2014-03-25 22:01:47 +0200

    Initial commit

---
+++ FastReader/FastReader/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FastReader
+//
+//  Created by Max Tymchii on 3/25/14.
+//  Copyright (c) 2014 Sebek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
